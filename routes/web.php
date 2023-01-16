c101f85d9db3f339b23bbf978e6d391cc59aea93
Gammy Denis <linodenis068@gmail.com>
2023-01-17 02:28:31 +0800

    about page updated

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AboutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+ Route::get('/about',[AboutController::class,'about']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
